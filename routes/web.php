ec6564a80cab44750fb3472a01a25ab6afa79c38
sunzhengyang <915814695@qq.com>
2019-02-20 07:35:38 +0000

    集成支付宝支付

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
+Route::get('alipay', function() {
+    return app('alipay')->web([
+        'out_trade_no' => time(),
+        'total_amount' => '1',
+        'subject' => 'test subject - 测试',
+    ]);
+});
+
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show')->where(['product'=>'[0-9]+']);
