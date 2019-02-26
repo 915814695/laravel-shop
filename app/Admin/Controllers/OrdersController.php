34fb46ca196a907d59bdee70e5ed00661c7964e1
sunzhengyang <915814695@qq.com>
2019-02-26 02:41:27 +0000

    管理后台--添加订单详情

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -18,6 +18,14 @@ class OrdersController extends Controller
             ->header('订单列表')
             ->body($this->grid());
     }
+	
+	    public function show(Order $order, Content $content)
+    {
+        return $content
+            ->header('查看订单')
+            // body 方法可以接受 Laravel 的视图作为参数
+            ->body(view('admin.orders.show', ['order' => $order]));
+    }
 
     protected function grid()
     {
